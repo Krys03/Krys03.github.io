a4b158df4620654939567307850b96c4f601dbcc
Krys03 <61270901+Krys03@users.noreply.github.com>
2020-09-30 22:11:14 +0200

    add repo

---
+++ precache-manifest.ce51c8616705637ea6ed1628982872ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ba557da1fdb274cf8b7b8dd3415bce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1504b872424fc7912331",
+    "url": "./static/css/main.4653bcc0.chunk.css"
+  },
+  {
+    "revision": "641e88d8ecce64b1dea6",
+    "url": "./static/js/2.62ce9de3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.62ce9de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1504b872424fc7912331",
+    "url": "./static/js/main.fb12f3a2.chunk.js"
+  },
+  {
+    "revision": "30737dacb3c362d1e965",
+    "url": "./static/js/runtime-main.5074ded4.js"
+  }
+]);
